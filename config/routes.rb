d19d3453fe5594c60de9263f5b1c2ba806775bc9
MelsenC <melsen.carlsburg@gmail.com>
2022-11-21 16:19:04 -0500

    Add CRUD actions to Customers Controller, add
    resources :customers to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'customers', to: 'customers#index'
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
