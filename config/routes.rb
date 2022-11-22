e7fde6986d90c5cd304a158c38dbd81e8ccb2041
MelsenC <melsen.carlsburg@gmail.com>
2022-11-21 20:57:30 -0500

    Add routes for designs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :customers
+  resources :customers do
+    resources :designs
+  end
+
+  resources :designs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
