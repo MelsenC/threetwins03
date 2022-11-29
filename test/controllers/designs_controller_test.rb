d5e9cb1b3e7ecd73642b7cdcf79cd0462ee86b07
MelsenC <melsen.carlsburg@gmail.com>
2022-11-29 14:03:01 -0500

    Remove /test folder to get ready for installing
    rspec-rails

--- test/controllers/designs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class DesignsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get designs_index_url
-    assert_response :success
-  end
-end
