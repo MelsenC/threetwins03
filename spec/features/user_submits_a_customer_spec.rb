f0db9304e5ee942c40d25d8e537c6887f39148bc
MelsenC <melsen.carlsburg@gmail.com>
2022-12-12 12:40:31 -0500

    fix user_submits_a_customer_spec
    - added sign_in function, split into two scenarios

--- spec/features/user_submits_a_customer_spec.rb
+++ spec/features/user_submits_a_customer_spec.rb
@@ -1,14 +1,22 @@
 require "rails_helper"
 
 RSpec.feature "User submits a customer" do
-  scenario "they see the page for the submitted customer" do
+
+  scenario "they visit the Customers page successfully" do
+    sign_in
+    visit customers_path
+    expect(page).to have_css "h1", text: "Customers"
+  end
+
+  scenario "they submit a new customer successfully" do
+    sign_in
+    visit customers_path
     customer_code = "ZZ"
     customer_name = "Sleepers R Us"
-    visit customers_path
     click_on "New Customer"
     fill_in "Code", with: customer_code
     fill_in "Name", with: customer_name
     click_on "Create Customer"
-    expect(page).to have_text("ZZ - Sleepers R Us")
+    expect(page).to have_css "h1", text: "ZZ - Sleepers R Us"
   end
 end
