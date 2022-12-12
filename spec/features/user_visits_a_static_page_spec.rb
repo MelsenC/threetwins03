aae7118f11a1afee5bcba3c18422d0a42133624a
MelsenC <melsen.carlsburg@gmail.com>
2022-12-12 12:41:39 -0500

    fix user_visits_a_static_page_spec
    - it now visits the correct paths
    - it now correctly searches for CSS

--- spec/features/user_visits_a_static_page_spec.rb
+++ spec/features/user_visits_a_static_page_spec.rb
@@ -2,28 +2,29 @@ require "rails_helper"
 
 RSpec.feature "User visits a static page" do
   scenario "when the user is on the home page, the nav link is active" do
-    visit root_path
+    visit home_path
     find('#navbarSupportedContent').click_link('Home')
-    expect(find('#nav_home')).to have_class('active')
+    expect(page).to have_css("a#nav_home", :class => "nav-link active")
   end
   scenario "when the user is on the services page, the nav link is active" do
-    visit root_path
+    visit services_path
     find('#navbarSupportedContent').click_link('Services')
-    expect(find('#nav_services')).to have_class('active')
+    expect(page).to have_css("a#nav_services", :class => "nav-link active")
   end
   scenario "when the user is on the gallery page, the nav link is active" do
-    visit root_path
+    visit gallery_path
     find('#navbarSupportedContent').click_link('Gallery')
-    expect(find('#nav_gallery')).to have_class('active')
+    expect(page).to have_css("a#nav_gallery", :class => "nav-link active")
   end
   scenario "when the user is on the PSST page, the nav link is active" do
-    visit root_path
+    visit psst_path
     find('#navbarSupportedContent').click_link('SanMar PSST Program')
-    expect(find('#nav_psst')).to have_class('active')
+    expect(page).to have_css("a#nav_psst", :class => "nav-link active")
   end
   scenario "when the user is on the Manage Orders page, the nav link is active" do
-    visit root_path
+    sign_in
+    visit manage_orders_path
     find('#navbarSupportedContent').click_link('Manage Orders')
-    expect(find('#nav_manage_orders')).to have_class('active')
+    expect(page).to have_css("a#nav_manage_orders", :class => "nav-link active")
   end
 end
